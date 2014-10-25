ba1be7677157d3b782ea98b9020802d47949e91e
gorhill <rhill@raymondhill.net>
2014-10-24 23:34:09 -0400

    need to be more specific about build

--- tools/make-chromium.sh
+++ tools/make-chromium.sh
@@ -4,9 +4,9 @@
 
 echo "*** µMatrix(Chromium): Creating package"
 echo "*** µMatrix(Chromium): Copying files"
-mkdir -p ./dist/chromium
-cp -R ./src/*           ./dist/chromium/
-cp -R ./tools/_locales  ./dist/chromium/
-cp -R ./assets          ./dist/chromium/
-cp    ./meta/chromium/* ./dist/chromium/
+mkdir -p ./dist/uMatrix.chromium
+cp -R ./src/*           ./dist/uMatrix.chromium/
+cp -R ./tools/_locales  ./dist/uMatrix.chromium/
+cp -R ./assets          ./dist/uMatrix.chromium/
+cp    ./meta/chromium/* ./dist/uMatrix.chromium/
 echo "*** µMatrix(Chromium): Package done."
