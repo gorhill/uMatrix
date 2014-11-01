8b02e6df7f270ebf355916a4c84b2c7e99a839c7
gorhill <rhill@raymondhill.net>
2014-11-01 10:43:47 -0400

    using variables

--- tools/import-crowdin.sh
+++ tools/import-crowdin.sh
@@ -2,59 +2,63 @@
 #
 # This script assumes a linux environment
 
-echo "*** uMatrix: Importing from Crowdin archive"
-rm -r ~/Downloads/crowdin
-unzip -q ~/Downloads/umatrix.zip -d ~/Downloads/crowdin
-cp ~/Downloads/crowdin/am/messages.json    ./tools/_locales/am/messages.json
-cp ~/Downloads/crowdin/ar/messages.json    ./tools/_locales/ar/messages.json
-cp ~/Downloads/crowdin/bg/messages.json    ./tools/_locales/bg/messages.json
-cp ~/Downloads/crowdin/bn/messages.json    ./tools/_locales/bn/messages.json
-cp ~/Downloads/crowdin/ca/messages.json    ./tools/_locales/ca/messages.json
-cp ~/Downloads/crowdin/cs/messages.json    ./tools/_locales/cs/messages.json
-cp ~/Downloads/crowdin/da/messages.json    ./tools/_locales/da/messages.json
-cp ~/Downloads/crowdin/de/messages.json    ./tools/_locales/de/messages.json
-cp ~/Downloads/crowdin/el/messages.json    ./tools/_locales/el/messages.json
-cp ~/Downloads/crowdin/en-GB/messages.json ./tools/_locales/en_GB/messages.json
-cp ~/Downloads/crowdin/es-ES/messages.json ./tools/_locales/es/messages.json
-cp ~/Downloads/crowdin/et/messages.json    ./tools/_locales/et/messages.json
-cp ~/Downloads/crowdin/fa/messages.json    ./tools/_locales/fa/messages.json
-cp ~/Downloads/crowdin/fi/messages.json    ./tools/_locales/fi/messages.json
-cp ~/Downloads/crowdin/fil/messages.json   ./tools/_locales/fil/messages.json
-cp ~/Downloads/crowdin/fr/messages.json    ./tools/_locales/fr/messages.json
-cp ~/Downloads/crowdin/gu-IN/messages.json ./tools/_locales/gu/messages.json
-cp ~/Downloads/crowdin/he/messages.json    ./tools/_locales/he/messages.json
-cp ~/Downloads/crowdin/hi/messages.json    ./tools/_locales/hi/messages.json
-cp ~/Downloads/crowdin/hr/messages.json    ./tools/_locales/hr/messages.json
-cp ~/Downloads/crowdin/hu/messages.json    ./tools/_locales/hu/messages.json
-cp ~/Downloads/crowdin/id/messages.json    ./tools/_locales/id/messages.json
-cp ~/Downloads/crowdin/it/messages.json    ./tools/_locales/it/messages.json
-cp ~/Downloads/crowdin/ja/messages.json    ./tools/_locales/ja/messages.json
-cp ~/Downloads/crowdin/kn/messages.json    ./tools/_locales/kn/messages.json
-cp ~/Downloads/crowdin/ko/messages.json    ./tools/_locales/ko/messages.json
-cp ~/Downloads/crowdin/lt/messages.json    ./tools/_locales/lt/messages.json
-cp ~/Downloads/crowdin/lv/messages.json    ./tools/_locales/lv/messages.json
-cp ~/Downloads/crowdin/ml-IN/messages.json ./tools/_locales/ml/messages.json
-cp ~/Downloads/crowdin/mr/messages.json    ./tools/_locales/mr/messages.json
-cp ~/Downloads/crowdin/ms/messages.json    ./tools/_locales/ms/messages.json
-cp ~/Downloads/crowdin/nl/messages.json    ./tools/_locales/nl/messages.json
-cp ~/Downloads/crowdin/no/messages.json    ./tools/_locales/nb/messages.json
-cp ~/Downloads/crowdin/pl/messages.json    ./tools/_locales/pl/messages.json
-cp ~/Downloads/crowdin/pt-BR/messages.json ./tools/_locales/pt_BR/messages.json
-cp ~/Downloads/crowdin/pt-PT/messages.json ./tools/_locales/pt_PT/messages.json
-cp ~/Downloads/crowdin/ro/messages.json    ./tools/_locales/ro/messages.json
-cp ~/Downloads/crowdin/ru/messages.json    ./tools/_locales/ru/messages.json
-cp ~/Downloads/crowdin/sk/messages.json    ./tools/_locales/sk/messages.json
-cp ~/Downloads/crowdin/sl/messages.json    ./tools/_locales/sl/messages.json
-cp ~/Downloads/crowdin/sr-CS/messages.json ./tools/_locales/sr/messages.json
-cp ~/Downloads/crowdin/sv-SE/messages.json ./tools/_locales/sv/messages.json
-cp ~/Downloads/crowdin/sw/messages.json    ./tools/_locales/sw/messages.json
-cp ~/Downloads/crowdin/ta/messages.json    ./tools/_locales/ta/messages.json
-cp ~/Downloads/crowdin/te/messages.json    ./tools/_locales/te/messages.json
-cp ~/Downloads/crowdin/th/messages.json    ./tools/_locales/th/messages.json
-cp ~/Downloads/crowdin/tr/messages.json    ./tools/_locales/tr/messages.json
-cp ~/Downloads/crowdin/uk/messages.json    ./tools/_locales/uk/messages.json
-cp ~/Downloads/crowdin/vi/messages.json    ./tools/_locales/vi/messages.json
-cp ~/Downloads/crowdin/zh-CN/messages.json ./tools/_locales/zh_CN/messages.json
+DLDIR=~/Downloads
+SRCDIR=$DLDIR/crowdin
+DESDIR=./tools/_locales
 
-rm -r ~/Downloads/crowdin
+echo "*** uMatrix: Importing from Crowdin archive"
+rm -r $SRCDIR
+unzip -q $DLDIR/umatrix.zip -d $SRCDIR
+cp $SRCDIR/am/messages.json    $DESDIR/am/messages.json
+cp $SRCDIR/ar/messages.json    $DESDIR/ar/messages.json
+cp $SRCDIR/bg/messages.json    $DESDIR/bg/messages.json
+cp $SRCDIR/bn/messages.json    $DESDIR/bn/messages.json
+cp $SRCDIR/ca/messages.json    $DESDIR/ca/messages.json
+cp $SRCDIR/cs/messages.json    $DESDIR/cs/messages.json
+cp $SRCDIR/da/messages.json    $DESDIR/da/messages.json
+cp $SRCDIR/de/messages.json    $DESDIR/de/messages.json
+cp $SRCDIR/el/messages.json    $DESDIR/el/messages.json
+cp $SRCDIR/en-GB/messages.json $DESDIR/en_GB/messages.json
+cp $SRCDIR/es-ES/messages.json $DESDIR/es/messages.json
+cp $SRCDIR/et/messages.json    $DESDIR/et/messages.json
+cp $SRCDIR/fa/messages.json    $DESDIR/fa/messages.json
+cp $SRCDIR/fi/messages.json    $DESDIR/fi/messages.json
+cp $SRCDIR/fil/messages.json   $DESDIR/fil/messages.json
+cp $SRCDIR/fr/messages.json    $DESDIR/fr/messages.json
+cp $SRCDIR/gu-IN/messages.json $DESDIR/gu/messages.json
+cp $SRCDIR/he/messages.json    $DESDIR/he/messages.json
+cp $SRCDIR/hi/messages.json    $DESDIR/hi/messages.json
+cp $SRCDIR/hr/messages.json    $DESDIR/hr/messages.json
+cp $SRCDIR/hu/messages.json    $DESDIR/hu/messages.json
+cp $SRCDIR/id/messages.json    $DESDIR/id/messages.json
+cp $SRCDIR/it/messages.json    $DESDIR/it/messages.json
+cp $SRCDIR/ja/messages.json    $DESDIR/ja/messages.json
+cp $SRCDIR/kn/messages.json    $DESDIR/kn/messages.json
+cp $SRCDIR/ko/messages.json    $DESDIR/ko/messages.json
+cp $SRCDIR/lt/messages.json    $DESDIR/lt/messages.json
+cp $SRCDIR/lv/messages.json    $DESDIR/lv/messages.json
+cp $SRCDIR/ml-IN/messages.json $DESDIR/ml/messages.json
+cp $SRCDIR/mr/messages.json    $DESDIR/mr/messages.json
+cp $SRCDIR/ms/messages.json    $DESDIR/ms/messages.json
+cp $SRCDIR/nl/messages.json    $DESDIR/nl/messages.json
+cp $SRCDIR/no/messages.json    $DESDIR/nb/messages.json
+cp $SRCDIR/pl/messages.json    $DESDIR/pl/messages.json
+cp $SRCDIR/pt-BR/messages.json $DESDIR/pt_BR/messages.json
+cp $SRCDIR/pt-PT/messages.json $DESDIR/pt_PT/messages.json
+cp $SRCDIR/ro/messages.json    $DESDIR/ro/messages.json
+cp $SRCDIR/ru/messages.json    $DESDIR/ru/messages.json
+cp $SRCDIR/sk/messages.json    $DESDIR/sk/messages.json
+cp $SRCDIR/sl/messages.json    $DESDIR/sl/messages.json
+cp $SRCDIR/sr-CS/messages.json $DESDIR/sr/messages.json
+cp $SRCDIR/sv-SE/messages.json $DESDIR/sv/messages.json
+cp $SRCDIR/sw/messages.json    $DESDIR/sw/messages.json
+cp $SRCDIR/ta/messages.json    $DESDIR/ta/messages.json
+cp $SRCDIR/te/messages.json    $DESDIR/te/messages.json
+cp $SRCDIR/th/messages.json    $DESDIR/th/messages.json
+cp $SRCDIR/tr/messages.json    $DESDIR/tr/messages.json
+cp $SRCDIR/uk/messages.json    $DESDIR/uk/messages.json
+cp $SRCDIR/vi/messages.json    $DESDIR/vi/messages.json
+cp $SRCDIR/zh-CN/messages.json $DESDIR/zh_CN/messages.json
+
+rm -r $SRCDIR
 echo "*** uMatrix: Import done."
