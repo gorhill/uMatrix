10b708aee9daa143ae687d59fa881b5f7c7dba78
Raymond Hill <rhill@raymondhill.net>
2014-11-19 23:35:17 -0200

    clean before build

--- tools/make-chromium.sh
+++ tools/make-chromium.sh
@@ -4,9 +4,11 @@
 
 echo "*** µMatrix(Chromium): Creating package"
 echo "*** µMatrix(Chromium): Copying files"
-mkdir -p ./dist/uMatrix.chromium
-cp -R ./src/*           ./dist/uMatrix.chromium/
-cp -R ./tools/_locales  ./dist/uMatrix.chromium/
-cp -R ./assets          ./dist/uMatrix.chromium/
-cp    ./meta/chromium/* ./dist/uMatrix.chromium/
+DES=./dist/uMatrix.chromium
+rm -rf $DES
+mkdir -p $DES
+cp -R ./src/*           $DES
+cp -R ./tools/_locales  $DES
+cp -R ./assets          $DES
+cp    ./meta/chromium/* $DES
 echo "*** µMatrix(Chromium): Package done."
