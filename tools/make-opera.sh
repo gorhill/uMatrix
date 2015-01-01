b1bc91f07070bd7c9aaa6ec9f7f9377de48f84dc
gorhill <rhill@raymondhill.net>
2015-01-01 12:02:32 -0500

    minor edits

--- tools/make-opera.sh
+++ tools/make-opera.sh
@@ -4,17 +4,19 @@
 
 echo "*** µMatrix(Opera): Creating package"
 echo "*** µMatrix(Opera): Copying files"
-mkdir -p ./dist/uMatrix.opera
-cp -R ./src/*           ./dist/uMatrix.opera/
-cp -R ./assets          ./dist/uMatrix.opera/
-cp    ./meta/chromium/* ./dist/uMatrix.opera/
+DES=./dist/uMatrix.opera
+rm -r $DES
+mkdir -p $DES
+cp -R ./src/*           $DES/
+cp -R ./assets          $DES/
+cp    ./meta/chromium/* $DES/
 # Copy only locales with fully translated description
-mkdir -p ./dist/uMatrix.opera/_locales
-cp -R ./tools/_locales/de    ./dist/uMatrix.opera/_locales/
-cp -R ./tools/_locales/en    ./dist/uMatrix.opera/_locales/
-cp -R ./tools/_locales/es    ./dist/uMatrix.opera/_locales/
-cp -R ./tools/_locales/fr    ./dist/uMatrix.opera/_locales/
-cp -R ./tools/_locales/he    ./dist/uMatrix.opera/_locales/
-cp -R ./tools/_locales/pt_BR ./dist/uMatrix.opera/_locales/
-cp -R ./tools/_locales/tr    ./dist/uMatrix.opera/_locales/
+mkdir -p $DES/_locales
+cp -R ./tools/_locales/de    $DES/_locales/
+cp -R ./tools/_locales/en    $DES/_locales/
+cp -R ./tools/_locales/es    $DES/_locales/
+cp -R ./tools/_locales/fr    $DES/_locales/
+cp -R ./tools/_locales/he    $DES/_locales/
+cp -R ./tools/_locales/pt_BR $DES/_locales/
+cp -R ./tools/_locales/tr    $DES/_locales/
 echo "*** µMatrix(Opera): Package done."
