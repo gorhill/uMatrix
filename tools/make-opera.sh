caeb0973d42c54d08f52921277dda2c3a35b8535
gorhill <rhill@raymondhill.net>
2015-05-21 20:11:21 -0400

    updated opera package script

--- tools/make-opera.sh
+++ tools/make-opera.sh
@@ -2,21 +2,32 @@
 #
 # This script assumes a linux environment
 
-echo "*** µMatrix(Opera): Creating package"
-echo "*** µMatrix(Opera): Copying files"
-DES=./dist/uMatrix.opera
+echo "*** uMatrix(Opera): Creating package"
+echo "*** uMatrix(Opera): Copying files"
+
+DES=./dist/build/uMatrix.opera
 rm -r $DES
 mkdir -p $DES
-cp -R ./src/*           $DES/
-cp -R ./assets          $DES/
-cp    ./meta/chromium/* $DES/
+
+cp -R ./assets                          $DES/
+cp -R ./src/*                           $DES/
+cp    ./platform/chromium/*.html        $DES/
+cp    ./platform/chromium/*.js          $DES/js/
+cp -R ./platform/chromium/img/*         $DES/img/
+cp    ./platform/chromium/manifest.json $DES/
+cp    LICENSE.txt                       $DES/
+
 # Copy only locales with fully translated description
+rm   -rf $DES/_locales
 mkdir -p $DES/_locales
-cp -R ./tools/_locales/de    $DES/_locales/
-cp -R ./tools/_locales/en    $DES/_locales/
-cp -R ./tools/_locales/es    $DES/_locales/
-cp -R ./tools/_locales/fr    $DES/_locales/
-cp -R ./tools/_locales/he    $DES/_locales/
-cp -R ./tools/_locales/pt_BR $DES/_locales/
-cp -R ./tools/_locales/tr    $DES/_locales/
-echo "*** µMatrix(Opera): Package done."
+cp -R ./src/_locales/de    $DES/_locales/
+cp -R ./src/_locales/en    $DES/_locales/
+cp -R ./src/_locales/es    $DES/_locales/
+cp -R ./src/_locales/fr    $DES/_locales/
+cp -R ./src/_locales/he    $DES/_locales/
+cp -R ./src/_locales/id    $DES/_locales/
+cp -R ./src/_locales/it    $DES/_locales/
+cp -R ./src/_locales/pt_BR $DES/_locales/
+cp -R ./src/_locales/pt_PT $DES/_locales/
+cp -R ./src/_locales/tr    $DES/_locales/
+echo "*** uMatrix(Opera): Package done."
