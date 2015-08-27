99225e1688244de41ee76095367234560497617b
gorhill <rhill@raymondhill.net>
2015-08-27 13:12:34 -0400

    ability to create a dev build for Firefox

--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -8,21 +8,28 @@ DES=dist/build/uMatrix.firefox
 rm -rf $DES
 mkdir -p $DES
 
-cp -R ./assets                          $DES/
-cp -R ./src/*                           $DES/
-mv     $DES/img/icon_128.png            $DES/icon.png
-cp    ./platform/firefox/vapi-*.js      $DES/js/
-cp    ./platform/firefox/bootstrap.js   $DES/
-cp    ./platform/firefox/frame*.js      $DES/
-cp -R ./platform/chromium/img           $DES/
-cp    ./platform/firefox/css/*          $DES/css/
-cp    ./platform/firefox/chrome.manifest $DES/
-cp    ./platform/firefox/install.rdf    $DES/
-cp    ./platform/firefox/*.xul          $DES/
-cp    ./LICENSE.txt                     $DES/
+cp -R assets                            $DES/
+
+
+cp -R src/*                             $DES/
+
+
+
+
+
+mv    $DES/img/icon_128.png             $DES/icon.png
+cp    platform/firefox/css/*            $DES/css/
+cp    platform/firefox/vapi-*.js        $DES/js/
+cp    platform/firefox/bootstrap.js     $DES/
+cp    platform/firefox/frame*.js        $DES/
+cp -R platform/chromium/img             $DES/
+cp    platform/firefox/chrome.manifest  $DES/
+cp    platform/firefox/install.rdf      $DES/
+cp    platform/firefox/*.xul            $DES/
+cp    LICENSE.txt                       $DES/
 
 echo "*** uMatrix.firefox: Generating meta..."
-python tools/make-firefox-meta.py $DES/
+python tools/make-firefox-meta.py $DES/ "$2"
 
 if [ "$1" = all ]; then
     echo "*** uMatrix.firefox: Creating package..."
