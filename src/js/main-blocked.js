3f8168ce0bb7bb1837f9fedfc02aa09723ee3e25
gorhill <rhill@raymondhill.net>
2017-12-02 11:46:13 -0500

    fix #502

--- src/js/main-blocked.js
+++ src/js/main-blocked.js
@@ -1,7 +1,7 @@
 /*******************************************************************************
 
-    µMatrix - a browser extension to block requests.
-    Copyright (C) 2015 Raymond Hill
+    uMatrix - a browser extension to block requests.
+    Copyright (C) 2015-2017 Raymond Hill
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -33,16 +33,117 @@ var details = {};
 
 (function() {
     var matches = /details=([^&]+)/.exec(window.location.search);
-    if ( matches === null ) {
-        return;
+    if ( matches === null ) { return; }
+    try {
+        details = JSON.parse(atob(matches[1]));
+    } catch(ex) {
     }
-    details = JSON.parse(atob(matches[1]));
 })();
 
 /******************************************************************************/
 
 uDom('.what').text(details.url);
-uDom('#why').text(details.why.slice(3));
+// uDom('#why').text(details.why.slice(3));
+
+/******************************************************************************/
+
+// https://github.com/gorhill/uMatrix/issues/502
+//   Code below originally imported from:
+//   https://github.com/gorhill/uBlock/blob/master/src/js/document-blocked.js
+
+(function() {
+    if ( typeof URL !== 'function' ) { return; }
+
+    var reURL = /^https?:\/\//;
+
+    var liFromParam = function(name, value) {
+        if ( value === '' ) {
+            value = name;
+            name = '';
+        }
+        var li = document.createElement('li');
+        var span = document.createElement('span');
+        span.textContent = name;
+        li.appendChild(span);
+        if ( name !== '' && value !== '' ) {
+            li.appendChild(document.createTextNode(' = '));
+        }
+        span = document.createElement('span');
+        if ( reURL.test(value) ) {
+            var a = document.createElement('a');
+            a.href = a.textContent = value;
+            span.appendChild(a);
+        } else {
+            span.textContent = value;
+        }
+        li.appendChild(span);
+        return li;
+    };
+
+    var safeDecodeURIComponent = function(s) {
+        try {
+            s = decodeURIComponent(s);
+        } catch (ex) {
+        }
+        return s;
+    };
+
+    var renderParams = function(parentNode, rawURL) {
+        var a = document.createElement('a');
+        a.href = rawURL;
+        if ( a.search.length === 0 ) { return false; }
+
+        var pos = rawURL.indexOf('?');
+        var li = liFromParam(
+            vAPI.i18n('docblockedNoParamsPrompt'),
+            rawURL.slice(0, pos)
+        );
+        parentNode.appendChild(li);
+
+        var params = a.search.slice(1).split('&');
+        var param, name, value, ul;
+        for ( var i = 0; i < params.length; i++ ) {
+            param = params[i];
+            pos = param.indexOf('=');
+            if ( pos === -1 ) {
+                pos = param.length;
+            }
+            name = safeDecodeURIComponent(param.slice(0, pos));
+            value = safeDecodeURIComponent(param.slice(pos + 1));
+            li = liFromParam(name, value);
+            if ( reURL.test(value) ) {
+                ul = document.createElement('ul');
+                renderParams(ul, value);
+                li.appendChild(ul);
+            }
+            parentNode.appendChild(li);
+        }
+        return true;
+    };
+
+    if ( renderParams(uDom.nodeFromId('parsed'), details.url) === false ) {
+        return;
+    }
+
+    var toggler = document.createElement('span');
+    toggler.className = 'fa';
+    uDom('#theURL > p').append(toggler);
+
+    uDom(toggler).on('click', function() {
+        var collapsed = uDom.nodeFromId('theURL').classList.toggle('collapsed');
+        vAPI.localStorage.setItem(
+            'document-blocked-collapse-url',
+            collapsed.toString()
+        );
+    });
+
+    uDom.nodeFromId('theURL').classList.toggle(
+        'collapsed',
+        vAPI.localStorage.getItem('document-blocked-collapse-url') === 'true'
+    );
+})();
+
+/******************************************************************************/
 
 if ( window.history.length > 1 ) {
     uDom('#back').on('click', function() { window.history.back(); });
