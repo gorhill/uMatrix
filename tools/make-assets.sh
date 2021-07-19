9d97748772b66e59a3394da5050cc4535094ef26
Raymond Hill <rhill@raymondhill.net>
2021-07-19 10:20:05 -0400

    Fix fetching of assets at build time

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -14,11 +14,14 @@ cp ./assets/assets.json $DES/
 if [ -n "${TRAVIS_TAG}" ]; then
   pushd .. > /dev/null
   git clone --depth 1 https://github.com/uBlockOrigin/uAssets.git
+  git checkout 84dc2761abb4193bb34290aa6d90266610f735f6
   popd > /dev/null
 fi
 
 mkdir $DES/thirdparties
-cp -R ../uAssets/thirdparties/hosts-file.net             $DES/thirdparties/
+pushd ../uAssets
+git checkout 84dc2761abb4193bb34290aa6d90266610f735f6
+popd
 cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
 cp -R ../uAssets/thirdparties/pgl.yoyo.org               $DES/thirdparties/
 cp -R ../uAssets/thirdparties/publicsuffix.org           $DES/thirdparties/
@@ -27,5 +30,8 @@ cp -R ../uAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
 cp -R ../uAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
 mkdir $DES/umatrix
 cp -R ../uAssets/recipes/*                               $DES/umatrix/
+pushd ../uAssets
+git checkout master
+popd
 
 echo "done."
