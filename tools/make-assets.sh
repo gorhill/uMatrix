13c45f12244453e27383c9041a76194831d81ef7
Raymond Hill <rhill@raymondhill.net>
2021-07-20 06:53:24 -0400

    Remove obsolete assets

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -22,12 +22,10 @@ mkdir $DES/thirdparties
 pushd ../uAssets
 git checkout 84dc2761abb4193bb34290aa6d90266610f735f6
 popd
-cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
 cp -R ../uAssets/thirdparties/pgl.yoyo.org               $DES/thirdparties/
 cp -R ../uAssets/thirdparties/publicsuffix.org           $DES/thirdparties/
 cp -R ../uAssets/thirdparties/someonewhocares.org        $DES/thirdparties/
 cp -R ../uAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
-cp -R ../uAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
 mkdir $DES/umatrix
 cp -R ../uAssets/recipes/*                               $DES/umatrix/
 pushd ../uAssets
